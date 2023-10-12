18735b6528b139277a7651a01316b12580cc0647
Dung Ken Dev <vincentcode402112@gmail.com>
2023-10-12 12:57:07 +0700

    upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Bạn';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100050175040333';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Diễm Quỳnh';
+const contentCard = 'Chúc bạn sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'avatar.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
